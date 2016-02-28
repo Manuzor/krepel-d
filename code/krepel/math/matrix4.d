f26ddf5b5654403ec2449b7985c2da145f674c81
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-02-28 17:03:47 +0100

    Adds Matrix Inversion, Split GetAxis and adds InverseTransform of Vectors.

--- code/krepel/math/matrix4.d
+++ code/krepel/math/matrix4.d
@@ -52,7 +52,7 @@ float GetDeterminant(Matrix4 Mat)
   detA = a|j, k, l|
           |n, o ,p|
   */
-  float detA = Mat.M[0][0] * (
+  float DetA = Mat.M[0][0] * (
     (Mat.M[1][1] * (Mat.M[2][2] * Mat.M[3][3] - Mat.M[2][3] * Mat.M[3][2])) -
     (Mat.M[1][2] * (Mat.M[2][1] * Mat.M[3][3] - Mat.M[2][3] * Mat.M[3][1])) +
     (Mat.M[1][3] * (Mat.M[2][1] * Mat.M[3][2] - Mat.M[2][2] * Mat.M[3][1]))
@@ -60,10 +60,10 @@ float GetDeterminant(Matrix4 Mat)
 
   /*
           |e, g, h|
-  detB = b|i, k, l|
+  DetB = b|i, k, l|
           |m, o ,p|
   */
-  float detB = Mat.M[0][1] * (
+  float DetB = Mat.M[0][1] * (
     (Mat.M[1][0] * (Mat.M[2][2] * Mat.M[3][3] - Mat.M[2][3] * Mat.M[3][2])) -
     (Mat.M[1][2] * (Mat.M[2][0] * Mat.M[3][3] - Mat.M[2][3] * Mat.M[3][0])) +
     (Mat.M[1][3] * (Mat.M[2][0] * Mat.M[3][2] - Mat.M[2][2] * Mat.M[3][0]))
@@ -71,10 +71,10 @@ float GetDeterminant(Matrix4 Mat)
 
   /*
           |e, f, h|
-  detC = c|i, j, l|
+  DetC = c|i, j, l|
           |m, n ,p|
   */
-  float detC = Mat.M[0][2] * (
+  float DetC = Mat.M[0][2] * (
     (Mat.M[1][0] * (Mat.M[2][1] * Mat.M[3][3] - Mat.M[2][3] * Mat.M[3][1])) -
     (Mat.M[1][1] * (Mat.M[2][0] * Mat.M[3][3] - Mat.M[2][3] * Mat.M[3][0])) +
     (Mat.M[1][3] * (Mat.M[2][0] * Mat.M[3][1] - Mat.M[2][1] * Mat.M[3][0]))
@@ -82,22 +82,109 @@ float GetDeterminant(Matrix4 Mat)
 
   /*
           |e, f, g|
-  detD = d|i, j, k|
+  DetD = d|i, j, k|
           |m, n ,o|
   */
-  float detD = Mat.M[0][3] * (
+  float DetD = Mat.M[0][3] * (
     (Mat.M[1][0] * (Mat.M[2][1] * Mat.M[3][2] - Mat.M[2][2] * Mat.M[3][1])) -
     (Mat.M[1][1] * (Mat.M[2][0] * Mat.M[3][2] - Mat.M[2][2] * Mat.M[3][0])) +
     (Mat.M[1][2] * (Mat.M[2][0] * Mat.M[3][1] - Mat.M[2][1] * Mat.M[3][0]))
   );
 
-  return detA - detB + detC - detD;
+  return DetA - DetB + DetC - DetD;
+}
+
+/// Calculcates the inversion of the matrix, if possible, otherwise return the identity matrix.
+/// Additionally checks for Zero scale matrix and returns an Identity Matrix in this case as well.
+Matrix4 SafeInvert(Matrix4 Mat)
+{
+  if (!Mat.IsInvertible() || (
+      Mat.GetScaledAxis(EAxisType.X).IsNearlyZero &&
+      Mat.GetScaledAxis(EAxisType.Y).IsNearlyZero &&
+      Mat.GetScaledAxis(EAxisType.Z).IsNearlyZero
+    ))
+  {
+    return Matrix4.Identity;
+  }
+  else
+  {
+    return UnsafeInvert(Mat);
+  }
+}
+
+/// Runs a Matrix inversion
+/// Checks in debug build if it can be inverted and raises an assert if matrix cannot be inverted.
+/// No checks in release build.
+Matrix4 UnsafeInvert(Matrix4 Mat)
+{
+  Matrix4 Inverted;
+  const float InvDet = 1/Mat.GetDeterminant();
+
+  debug
+  {
+    assert((!Mat.IsInvertible() || (
+        Mat.GetScaledAxis(EAxisType.X).IsNearlyZero &&
+        Mat.GetScaledAxis(EAxisType.Y).IsNearlyZero &&
+        Mat.GetScaledAxis(EAxisType.Z).IsNearlyZero)
+      ) == false, "Matrix can not be inverted, you should use SafeInvert or no Inversion at all.");
+  }
+
+  Inverted.M[0][0] = InvDet * (
+    Mat.M[1][2] * Mat.M[2][3] * Mat.M[3][1] - Mat.M[1][3] * Mat.M[2][2] * Mat.M[3][1] + Mat.M[1][3] * Mat.M[2][1] * Mat.M[3][2] -
+    Mat.M[1][1] * Mat.M[2][3] * Mat.M[3][2] - Mat.M[1][2] * Mat.M[2][1] * Mat.M[3][3] + Mat.M[1][1] * Mat.M[2][2] * Mat.M[3][3]);
+  Inverted.M[0][1] = InvDet * (
+    Mat.M[0][3] * Mat.M[2][2] * Mat.M[3][1] - Mat.M[0][2] * Mat.M[2][3] * Mat.M[3][1] - Mat.M[0][3] * Mat.M[2][1] * Mat.M[3][2] +
+    Mat.M[0][1] * Mat.M[2][3] * Mat.M[3][2] + Mat.M[0][2] * Mat.M[2][1] * Mat.M[3][3] - Mat.M[0][1] * Mat.M[2][2] * Mat.M[3][3]);
+  Inverted.M[0][2] = InvDet * (
+    Mat.M[0][2] * Mat.M[1][3] * Mat.M[3][1] - Mat.M[0][3] * Mat.M[1][2] * Mat.M[3][1] + Mat.M[0][3] * Mat.M[1][1] * Mat.M[3][2] -
+    Mat.M[0][1] * Mat.M[1][3] * Mat.M[3][2] - Mat.M[0][2] * Mat.M[1][1] * Mat.M[3][3] + Mat.M[0][1] * Mat.M[1][2] * Mat.M[3][3]);
+  Inverted.M[0][3] = InvDet * (
+    Mat.M[0][3] * Mat.M[1][2] * Mat.M[2][1] - Mat.M[0][2] * Mat.M[1][3] * Mat.M[2][1] - Mat.M[0][3] * Mat.M[1][1] * Mat.M[2][2] +
+    Mat.M[0][1] * Mat.M[1][3] * Mat.M[2][2] + Mat.M[0][2] * Mat.M[1][1] * Mat.M[2][3] - Mat.M[0][1] * Mat.M[1][2] * Mat.M[2][3]);
+  Inverted.M[1][0] = InvDet * (
+    Mat.M[1][3] * Mat.M[2][2] * Mat.M[3][0] - Mat.M[1][2] * Mat.M[2][3] * Mat.M[3][0] - Mat.M[1][3] * Mat.M[2][0] * Mat.M[3][2] +
+    Mat.M[1][0] * Mat.M[2][3] * Mat.M[3][2] + Mat.M[1][2] * Mat.M[2][0] * Mat.M[3][3] - Mat.M[1][0] * Mat.M[2][2] * Mat.M[3][3]);
+  Inverted.M[1][1] = InvDet * (
+    Mat.M[0][2] * Mat.M[2][3] * Mat.M[3][0] - Mat.M[0][3] * Mat.M[2][2] * Mat.M[3][0] + Mat.M[0][3] * Mat.M[2][0] * Mat.M[3][2] -
+    Mat.M[0][0] * Mat.M[2][3] * Mat.M[3][2] - Mat.M[0][2] * Mat.M[2][0] * Mat.M[3][3] + Mat.M[0][0] * Mat.M[2][2] * Mat.M[3][3]);
+  Inverted.M[1][2] = InvDet * (
+    Mat.M[0][3] * Mat.M[1][2] * Mat.M[3][0] - Mat.M[0][2] * Mat.M[1][3] * Mat.M[3][0] - Mat.M[0][3] * Mat.M[1][0] * Mat.M[3][2] +
+    Mat.M[0][0] * Mat.M[1][3] * Mat.M[3][2] + Mat.M[0][2] * Mat.M[1][0] * Mat.M[3][3] - Mat.M[0][0] * Mat.M[1][2] * Mat.M[3][3]);
+  Inverted.M[1][3] = InvDet * (
+    Mat.M[0][2] * Mat.M[1][3] * Mat.M[2][0] - Mat.M[0][3] * Mat.M[1][2] * Mat.M[2][0] + Mat.M[0][3] * Mat.M[1][0] * Mat.M[2][2] -
+    Mat.M[0][0] * Mat.M[1][3] * Mat.M[2][2] - Mat.M[0][2] * Mat.M[1][0] * Mat.M[2][3] + Mat.M[0][0] * Mat.M[1][2] * Mat.M[2][3]);
+  Inverted.M[2][0] = InvDet * (
+    Mat.M[1][1] * Mat.M[2][3] * Mat.M[3][0] - Mat.M[1][3] * Mat.M[2][1] * Mat.M[3][0] + Mat.M[1][3] * Mat.M[2][0] * Mat.M[3][1] -
+    Mat.M[1][0] * Mat.M[2][3] * Mat.M[3][1] - Mat.M[1][1] * Mat.M[2][0] * Mat.M[3][3] + Mat.M[1][0] * Mat.M[2][1] * Mat.M[3][3]);
+  Inverted.M[2][1] = InvDet * (
+    Mat.M[0][3] * Mat.M[2][1] * Mat.M[3][0] - Mat.M[0][1] * Mat.M[2][3] * Mat.M[3][0] - Mat.M[0][3] * Mat.M[2][0] * Mat.M[3][1] +
+    Mat.M[0][0] * Mat.M[2][3] * Mat.M[3][1] + Mat.M[0][1] * Mat.M[2][0] * Mat.M[3][3] - Mat.M[0][0] * Mat.M[2][1] * Mat.M[3][3]);
+  Inverted.M[2][2] = InvDet * (
+    Mat.M[0][1] * Mat.M[1][3] * Mat.M[3][0] - Mat.M[0][3] * Mat.M[1][1] * Mat.M[3][0] + Mat.M[0][3] * Mat.M[1][0] * Mat.M[3][1] -
+    Mat.M[0][0] * Mat.M[1][3] * Mat.M[3][1] - Mat.M[0][1] * Mat.M[1][0] * Mat.M[3][3] + Mat.M[0][0] * Mat.M[1][1] * Mat.M[3][3]);
+  Inverted.M[2][3] = InvDet * (
+    Mat.M[0][3] * Mat.M[1][1] * Mat.M[2][0] - Mat.M[0][1] * Mat.M[1][3] * Mat.M[2][0] - Mat.M[0][3] * Mat.M[1][0] * Mat.M[2][1] +
+    Mat.M[0][0] * Mat.M[1][3] * Mat.M[2][1] + Mat.M[0][1] * Mat.M[1][0] * Mat.M[2][3] - Mat.M[0][0] * Mat.M[1][1] * Mat.M[2][3]);
+  Inverted.M[3][0] = InvDet * (
+    Mat.M[1][2] * Mat.M[2][1] * Mat.M[3][0] - Mat.M[1][1] * Mat.M[2][2] * Mat.M[3][0] - Mat.M[1][2] * Mat.M[2][0] * Mat.M[3][1] +
+    Mat.M[1][0] * Mat.M[2][2] * Mat.M[3][1] + Mat.M[1][1] * Mat.M[2][0] * Mat.M[3][2] - Mat.M[1][0] * Mat.M[2][1] * Mat.M[3][2]);
+  Inverted.M[3][1] = InvDet * (
+    Mat.M[0][1] * Mat.M[2][2] * Mat.M[3][0] - Mat.M[0][2] * Mat.M[2][1] * Mat.M[3][0] + Mat.M[0][2] * Mat.M[2][0] * Mat.M[3][1] -
+    Mat.M[0][0] * Mat.M[2][2] * Mat.M[3][1] - Mat.M[0][1] * Mat.M[2][0] * Mat.M[3][2] + Mat.M[0][0] * Mat.M[2][1] * Mat.M[3][2]);
+  Inverted.M[3][2] = InvDet * (
+    Mat.M[0][2] * Mat.M[1][1] * Mat.M[3][0] - Mat.M[0][1] * Mat.M[1][2] * Mat.M[3][0] - Mat.M[0][2] * Mat.M[1][0] * Mat.M[3][1] +
+    Mat.M[0][0] * Mat.M[1][2] * Mat.M[3][1] + Mat.M[0][1] * Mat.M[1][0] * Mat.M[3][2] - Mat.M[0][0] * Mat.M[1][1] * Mat.M[3][2]);
+  Inverted.M[3][3] = InvDet * (
+    Mat.M[0][1] * Mat.M[1][2] * Mat.M[2][0] - Mat.M[0][2] * Mat.M[1][1] * Mat.M[2][0] + Mat.M[0][2] * Mat.M[1][0] * Mat.M[2][1] -
+    Mat.M[0][0] * Mat.M[1][2] * Mat.M[2][1] - Mat.M[0][1] * Mat.M[1][0] * Mat.M[2][2] + Mat.M[0][0] * Mat.M[1][1] * Mat.M[2][2]);
+
+  return Inverted;
 }
 
 /// Checks if the matrix can be inverted (if the Determinant is 1)
 bool IsInvertible(Matrix4 Mat)
 {
-  return NearlyEquals(Mat.GetDeterminant(),1);
+  return !NearlyEquals(Mat.GetDeterminant(),0);
 }
 
 /// Homogenous transform of a 4 dimensional Vector
@@ -119,6 +206,43 @@ Vector4 TransformVector(Matrix4 Mat, Vector4 Vector)
 Vector3 TransformPosition(Matrix4 Mat, Vector3 Vector)
 {
   return TransformVector(Mat, Vector.XYZ1).XYZ;
+}
+
+/// Transforms a direction, not taking the translation part of the matrix into account
+/// @return Result = Vector.XYZ1*Mat
+Vector3 TransformVector(Matrix4 Mat, Vector3 Vector)
+{
+  return TransformVector(Mat, Vector.XYZ0).XYZ;
+}
+
+/// Homogenous inverse transform of a 4 dimensional Vector
+/// @return Result = Vector*Mat^-1
+Vector4 InverseTransformVector(Matrix4 Mat, Vector4 Vector)
+{
+  Vector4 Result;
+
+  Matrix4 Inverted = Mat.SafeInvert();
+
+  Result.Data[0] = Inverted.M[0][0] * Vector.Data[0] + Inverted.M[1][0] * Vector.Data[1] + Inverted.M[2][0] * Vector.Data[2] + Inverted.M[3][0] * Vector.Data[3];
+  Result.Data[1] = Inverted.M[0][1] * Vector.Data[0] + Inverted.M[1][1] * Vector.Data[1] + Inverted.M[2][1] * Vector.Data[2] + Inverted.M[3][1] * Vector.Data[3];
+  Result.Data[2] = Inverted.M[0][2] * Vector.Data[0] + Inverted.M[1][2] * Vector.Data[1] + Inverted.M[2][2] * Vector.Data[2] + Inverted.M[3][2] * Vector.Data[3];
+  Result.Data[3] = Inverted.M[0][3] * Vector.Data[0] + Inverted.M[1][3] * Vector.Data[1] + Inverted.M[2][3] * Vector.Data[2] + Inverted.M[3][3] * Vector.Data[3];
+
+  return Result;
+}
+
+/// Homogenous inverse transform of a 3 dimensional Vector
+/// @return Result = Vector.XYZ1*Mat^-1
+Vector3 InverseTransformPosition(Matrix4 Mat, Vector3 Vector)
+{
+  return InverseTransformVector(Mat, Vector.XYZ1).XYZ;
+}
+
+/// InverseTransforms a direction, not taking the translation part of the matrix into account
+/// @return Result = Vector.XYZ1*Mat^-1
+Vector3 InverseTransformVector(Matrix4 Mat, Vector3 Vector)
+{
+  return InverseTransformVector(Mat, Vector.XYZ0).XYZ;
 }
 
 /// Returns transposed multiplication of the two matrices
@@ -156,7 +280,7 @@ enum EAxisType
   Z
 }
 
-Vector3 GetAxis(Matrix4 Mat, EAxisType Type)
+Vector3 GetScaledAxis(Matrix4 Mat, EAxisType Type)
 {
   final switch(Type)
   {
@@ -166,6 +290,20 @@ Vector3 GetAxis(Matrix4 Mat, EAxisType Type)
     return Vector3(Mat.M[1][0],Mat.M[1][1],Mat.M[1][2]);
   case EAxisType.Z:
     return Vector3(Mat.M[2][0],Mat.M[2][1],Mat.M[2][2]);
+  }
+  assert(false, "No Valid Axis Value");
+}
+
+Vector3 GetUnitAxis(Matrix4 Mat, EAxisType Type)
+{
+  final switch(Type)
+  {
+  case EAxisType.X:
+    return Vector3(Mat.M[0][0],Mat.M[0][1],Mat.M[0][2]).SafeNormalizedCopy();
+  case EAxisType.Y:
+    return Vector3(Mat.M[1][0],Mat.M[1][1],Mat.M[1][2]).SafeNormalizedCopy();
+  case EAxisType.Z:
+    return Vector3(Mat.M[2][0],Mat.M[2][1],Mat.M[2][2]).SafeNormalizedCopy();
   }
   assert(false, "No Valid Axis Value");
 }
@@ -274,7 +412,7 @@ unittest
   assert(ExpectedTranspose.M[3][3] == Transposed.M[3][3]);
 }
 
-/// GetAxis Value
+/// GetScaledAxis Value
 unittest
 {
   Matrix4 Mat = Matrix4([
@@ -282,9 +420,22 @@ unittest
     [ 5, 6, 7, 8],
     [ 9,10,11,12],
     [13,14,15,16]]);
-  assert(Mat.GetAxis(EAxisType.X) == Vector3(1,2,3));
-  assert(Mat.GetAxis(EAxisType.Y) == Vector3(5,6,7));
-  assert(Mat.GetAxis(EAxisType.Z) == Vector3(9,10,11));
+  assert(Mat.GetScaledAxis(EAxisType.X) == Vector3(1,2,3));
+  assert(Mat.GetScaledAxis(EAxisType.Y) == Vector3(5,6,7));
+  assert(Mat.GetScaledAxis(EAxisType.Z) == Vector3(9,10,11));
+}
+
+/// GetUnitAxis Value
+unittest
+{
+  Matrix4 Mat = Matrix4([
+    [ 5, 0, 0, 4],
+    [ 0, 6, 0, 8],
+    [ 0, 0,11,12],
+    [13,14,15,16]]);
+  assert(Mat.GetUnitAxis(EAxisType.X) == Vector3(1,0,0));
+  assert(Mat.GetUnitAxis(EAxisType.Y) == Vector3(0,1,0));
+  assert(Mat.GetUnitAxis(EAxisType.Z) == Vector3(0,0,1));
 }
 
 /// Get Translation
@@ -328,7 +479,7 @@ unittest
     assert(Expected.M[3][3] == Mat.M[3][3]);
 }
 
-/// Transform Position
+/// (Inverse)Transform Position/Vector
 unittest
 {
   // Rotate 90 Degrees CCW around the Z Axis
@@ -337,20 +488,31 @@ unittest
   Vector3 ExpectedPos = Vector3(0,-1,0);
 
   Vector3 Transformed = Mat.TransformPosition(Pos);
+  assert(ExpectedPos == Transformed);
+  Transformed = Mat.InverseTransformPosition(Transformed);
+  assert(Pos == Transformed);
 
-  assert(ExpectedPos.X == Transformed.X);
-  assert(ExpectedPos.Y == Transformed.Y);
-  assert(ExpectedPos.Z == Transformed.Z);
+  Transformed = Mat.TransformVector(Vector3.ForwardVector);
+  assert(Transformed == -Vector3.RightVector);
+  Transformed = Mat.InverseTransformVector(Transformed);
+  assert(Transformed == Vector3.ForwardVector);
 
   Mat = Matrix4(Vector3.ForwardVector, Vector3.RightVector, Vector3.UpVector, Vector3(10,20,50));
   Pos = Vector3(1,0,0);
   ExpectedPos = Vector3(11,20,50);
 
   Transformed = Mat.TransformPosition(Pos);
+  assert(ExpectedPos == Transformed);
+  Transformed = Mat.InverseTransformPosition(Transformed);
+  assert(Pos == Transformed);
 
-  assert(ExpectedPos.X == Transformed.X);
-  assert(ExpectedPos.Y == Transformed.Y);
-  assert(ExpectedPos.Z == Transformed.Z);
+  Transformed = Mat.TransformVector(Vector3.ForwardVector);
+  assert(Transformed == Vector3.ForwardVector);
+  Transformed = Mat.InverseTransformVector(Vector3.ForwardVector);
+  assert(Transformed == Vector3.ForwardVector);
+
+  assert(Mat.TransformVector(Vector4(0,0,0,2)) == Vector4(20, 40, 100, 2));
+  assert(Mat.InverseTransformVector(Mat.TransformVector(Vector4(0,0,0,2))) == Vector4(0, 0, 0, 2));
 
 }
 /// Determinant
@@ -366,8 +528,57 @@ unittest
     [13,14,15,16]]);
 
   assert(Mat.GetDeterminant() == 0);
+  assert(!Mat.IsInvertible());
 
   Mat = Matrix4(Vector3.ForwardVector, Vector3.RightVector, Vector3.UpVector, Vector3(10, 20, 50));
   assert(Mat.GetDeterminant() == 1);
   assert(Mat.IsInvertible());
+}
+
+/// Matrix multiplication
+unittest
+{
+  assert(Matrix4.Identity * Matrix4.Identity == Matrix4.Identity);
+
+  Matrix4 Mat = Matrix4([
+    [ 1, 2, 3, 4],
+    [ 5, 6, 7, 8],
+    [ 9,10,11,12],
+    [13,14,15,16]]);
+
+  assert(Matrix4.Identity * Mat == Mat);
+
+  Matrix4 Mat2 = Matrix4([
+    [16,15,14,13],
+    [12,11,10, 9],
+    [ 8, 7, 6, 5],
+    [ 4, 3, 2, 1]
+    ]);
+
+  Matrix4 Expected = Matrix4([
+    [80, 70, 60, 50],
+    [240, 214, 188, 162],
+    [400, 358, 316, 274],
+    [560, 502, 444, 386]
+    ]);
+
+  assert(Mat * Mat2 == Expected);
+}
+
+/// Matrix Inversion
+unittest
+{
+  assert(Matrix4.Identity.SafeInvert() == Matrix4.Identity);
+
+  Matrix4 Matrix = Matrix4([
+    [ 1, 2, 3, 4],
+    [ 5, 6, 7, 8],
+    [ 9,10,11,12],
+    [13,14,15,16]]);
+
+  assert(Matrix.SafeInvert() == Matrix4.Identity);
+
+  Matrix = Matrix4(Vector3.ForwardVector, Vector3.RightVector, Vector3.UpVector, Vector3(10, 20, 50));
+  assert(Matrix.IsInvertible);
+  assert(Matrix * Matrix.SafeInvert() == Matrix4.Identity);
 }
