3d65c2f74a9a7cae58bff68c6f32d791b36f4ed2
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-03-02 13:21:43 +0100

    Fixed bug in IsNaN check, made it cross platform.

--- code/krepel/math/math.d
+++ code/krepel/math/math.d
@@ -57,20 +57,7 @@ float ACos(float Value)
 /// Checks if the given float Value is QNaN
 bool IsNaN(float Value)
 {
-  bool Result;
-  asm @nogc @safe nothrow
-  {
-    fld Value; // Load Value to float stack ST(0)
-    ftst; // Compare ST(0) with 0.0
-    fstsw AX; // Write FPU Status Register to AX (16 Bit)
-    and AX, 0x4500; // Filter interesting bits (C0 C2 C3)
-    cmp AX, 0x4500; // Check if C0 C2 and C3 are set
-    lahf; // Load result of cmp into AH
-    and AH, 0x41; // Filter intereseting bits (CF, ZF)
-    xor AH, 0x40; // Check if ZF is set and CF is zero.
-    mov Result, AH; // Write result into bool variable
-  }
-  return !Result;
+  return Value!=Value;
 }
 
 /// Checks if a and b are equals with respect to some epsilon
