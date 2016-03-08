38346cca430a50715c208a39267f2fa0c86d24a9
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-03-08 14:13:05 +0100

    Adds IAllocator Interface to Open/CloseFile

--- code/krepel/win32/win32system.d
+++ code/krepel/win32/win32system.d
@@ -1,25 +1,25 @@
 module krepel.win32.system;
 
 import krepel.system;
+import krepel.memory;
 
 version(Windows):
 @nogc:
 nothrow:
 
 /// Creates and opens a file at the given path
-IFile OpenFile(const wchar[] Path, FileOpenMode Mode = FileOpenMode.Read)
+IFile OpenFile(IAllocator Allocator, const wchar[] Path, FileOpenMode Mode = FileOpenMode.Read)
 {
-  //TODO(Marvin): Create file using allocators
-  //auto NewFile = new Win32File();
-  //NewFile.OpenFile(Path, Mode);
+  auto NewFile = Allocator.New!Win32File();
+  NewFile.OpenFile(Path, Mode);
 
-  return null;
+  return NewFile;
 }
 
 /// Closes and destroys the file handle.
 /// Params:
 /// File = The file handle to destroy, the File is no longer valid after this call
-void CloseFile(IFile File)
+void CloseFile(IAllocator Allocator, IFile File)
 {
   Win32File WinFile = cast(Win32File)File;
   WinFile.CloseFile();
