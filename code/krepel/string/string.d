c26e5ab51cf97a9a556dfb094b7a975b7047ebe0
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-03-16 18:08:02 +0100

    Adds Find ReplaceFirst and ReplaceAll to String and Char Assignment.

--- code/krepel/string/string.d
+++ code/krepel/string/string.d
@@ -87,6 +87,53 @@ struct StringBase(CharType)
     }
   }
 
+  ulong Find(const(CharType[]) SearchString, ulong SearchStardIndex = 0) const
+  {
+    long SearchIndex = SearchStardIndex;
+    while(SearchString.length + SearchIndex <= Count)
+    {
+      if (this[SearchIndex .. SearchIndex + SearchString.length] == SearchString)
+      {
+        return SearchIndex;
+      }
+      SearchIndex++;
+    }
+    return -1;
+  }
+
+  bool ReplaceFirst(const(CharType[]) SearchString, const(CharType[]) ReplaceString)
+  {
+    long SearchIndex = Find(SearchString);
+    if (SearchIndex == -1)
+    {
+      return false;
+    }
+    EnsureSingleCopy();
+    Data.RemoveAt(SearchIndex, SearchString.length);
+    Data.Insert(SearchIndex, ReplaceString);
+    return true;
+  }
+
+  long ReplaceAll(const(CharType[]) SearchString, const(CharType[]) ReplaceString)
+  {
+    long StartSearchIndex = 0;
+    long SearchIndex = 0;
+    long ReplaceCount = 0;
+    while(SearchIndex >= 0)
+    {
+      SearchIndex = Find(SearchString, StartSearchIndex);
+      if (SearchIndex >= 0)
+      {
+        EnsureSingleCopy();
+        Data.RemoveAt(SearchIndex, SearchString.length);
+        Data.Insert(SearchIndex, ReplaceString);
+        StartSearchIndex = SearchIndex + ReplaceString.length;
+        ReplaceCount++;
+      }
+    }
+    return ReplaceCount;
+  }
+
   const(CharType[]) opIndex() const
   {
     return Data.Data[0..Count];
@@ -101,6 +148,22 @@ struct StringBase(CharType)
   {
     EnsureSingleCopy();
     Data.Data[Index] = Value;
+  }
+
+  void opAssign(const CharType[] Chars)
+  {
+    if (*RefCount == 1)
+    {
+      Allocator.Delete(RefCount);
+      RefCount = null;
+      Allocator.Delete!(Array!CharType)(Data);
+      Data = null;
+    }
+    else
+    {
+      (*RefCount)--;
+    }
+    this = StringBase!CharType(Chars);
   }
 
   ulong opDollar() const
@@ -159,5 +222,21 @@ unittest
   assert(AnotherString == "Thbs is a Test");
 
   assert(TestString.StartsWith("This"));
+  assert(!TestString.StartsWith("Test"));
   assert(TestString.EndsWith("Test"));
+  assert(!TestString.EndsWith("bar"));
+  assert(TestString.Find("is") == 2);
+  assert(TestString.Find(TestString) == 0);
+  assert(TestString.Find("foo") == -1);
+
+  assert(TestString.ReplaceFirst("This is a", "Yet another"));
+  assert(TestString == "Yet another Test");
+  assert(TestString.ReplaceAll("t", "f") ==  3);
+  assert(TestString == "Yef anofher Tesf");
+
+  TestString = "TESTESTEST";
+  assert(TestString.ReplaceAll("TEST", "TEST") == 2);
+  assert(TestString == "TESTESTEST");
+  assert(TestString.ReplaceAll("TEST", "FOO") == 2);
+  assert(TestString == "FOOESFOO");
 }
