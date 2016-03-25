95fe71f380971eaf3a71dc6e747e6ddb4f477757
Manuel Maier <mjmaier@gmx.de>
2016-03-26 00:17:00 +0100

    Enhanced `build clean`
    Shows what it cleans with -v and what it didn't clean with -vv now.

--- dev/clean.build.d
+++ dev/clean.build.d
@@ -7,23 +7,37 @@ immutable ThisDir = dirName(__FILE__);
 mixin AddBuildRule!("clean", BuildRule);
 
 
+immutable WhiteList = [ "*build.log", "*.sln", "*.vs" ];
+
+/// Clean out the build directory, without deleting the directory itself.
 void BuildRule(ref BuildContext Context)
 {
-  trace("Cleaning build files.");
+  auto BuildFiles = Context.BuildDir.dirEntries(SpanMode.shallow, false);
+  auto ToClean = BuildFiles.filter!(Entry => !WhiteList.canFind!((Pattern, File) => File.globMatch(Pattern))(Entry.name))
+                           .array;
 
-  // Clean out the build directory, but don't delete the directory itself.
-  auto BuildFiles = Context.BuildDir.dirEntries(SpanMode.shallow, false)
-                                    .filter!(a => !a.name.baseName.equal("build.log")); // Don't delete the build log file.
-  foreach(fileSystemItem; BuildFiles)
+  if(Context.Verbosity)
+  {
+    if(ToClean.empty) log("Nothing to clean.");
+    else              logf("Cleaning up:%-(\n  %s%)", ToClean);
+  }
+
+  foreach(fileSystemItem; ToClean)
   {
     if(fileSystemItem.isFile)
     {
-      fileSystemItem.name.remove();
+      remove(fileSystemItem.name);
     }
     else
     {
-      fileSystemItem.name.rmdirRecurse();
       assert(fileSystemItem.isDir);
+      rmdirRecurse(fileSystemItem.name);
     }
+  }
+
+  if(Context.Verbosity > 1)
+  {
+    auto Ignored = BuildFiles.filter!(File => !ToClean.canFind(File));
+    if(!Ignored.empty) logf("Ignored:%-(\n  %s%)", Ignored);
   }
 }
