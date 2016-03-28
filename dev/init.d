f1fdc0ad8c8b60972b8c822ebcbeca0820b20a1a
Manuel Maier <mjmaier@gmx.de>
2016-03-28 17:50:47 +0200

    Set some git config in init script.
    It's finally useful for something! :smile:

--- dev/init.d
+++ dev/init.d
@@ -15,11 +15,33 @@ pragma(msg, "! Compiling init script");
 
 // TODO: See below
 // What this script does:
-// - The equivalent to `mklink /L dmd2 C:\my\dir\to\dmd2
+// - Initialize the git repo for development.
+// - The equivalent to `mklink /J dmd2 C:\my\dir\to\dmd2
 // - Compile druntime
+
+// TODO: Will need to be platform specific.
+const GitExecutable = "git.exe";
+
+void initGit()
+{
+  // We 'wait()' here rather than letting the processes run in parallel to
+  // ensure potential console output is in sequential order.
+
+  // Turn off 'autocrlf' so git doesn't perform any line ending normalization by default.
+  spawnProcess([GitExecutable, "config", "core.autocrlf", "false"]).wait();
+
+  // Set the line endings used internally to 'lf'.
+  spawnProcess([GitExecutable, "config", "core.eol", "lf"]).wait();
+
+  version(Windows)
+  {
+    // Fix git trying to understand Windows file permissions.
+    spawnProcess([GitExecutable, "config", "core.filemode", "false"]).wait();
+  }
+}
 
 int main(string[] args)
 {
-  io.stderr.writeln("The init script is not implemented yet, sorry.");
-  return 1;
+  initGit();
+  return 0;
 }
