07b3e8051b3c305b20187c7825dcbaec68fd4c4d
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-04-10 22:57:37 +0200

    Adds transform to public import of krepel.math

--- code/krepel/math/package.d
+++ code/krepel/math/package.d
@@ -6,3 +6,4 @@ public import krepel.math.vector3;
 public import krepel.math.vector2;
 public import krepel.math.matrix4;
 public import krepel.math.quaternion;
+public import krepel.math.transform;
